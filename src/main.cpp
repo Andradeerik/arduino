7a35f9ce5548e5d7316558f76af9a27258a9a54e
Daniel <dani.erik18ap@gmail.com>
2019-05-22 10:52:48 -0500

    comentamos el loop

--- src/main.cpp
+++ src/main.cpp
@@ -25,35 +25,35 @@ void setup()
 void loop()
 {
 
-  val = ultraright.Ranging(CM); 
-  if ((val <= 15) && (old_val <= 15))
-  {
-    state = 1 - state;
-    delay(500);
-  }
-  old_val = val; 
-  if (state == 1)
-  {
-    digitalWrite(pinled, HIGH); 
-  }
-  else
-  {
-    digitalWrite(pinled, LOW);
-  }
+  // val = ultraright.Ranging(CM); 
+  // if ((val <= 15) && (old_val <= 15))
+  // {
+  //   state = 1 - state;
+  //   delay(500);
+  // }
+  // old_val = val; 
+  // if (state == 1)
+  // {
+  //   digitalWrite(pinled, HIGH); 
+  // }
+  // else
+  // {
+  //   digitalWrite(pinled, LOW);
+  // }
 
-  val2 = ultraleft.Ranging(CM); 
-  if ((val2 <= 15) && (old_val2 <= 15))
-  {
-    state2 = 1 - state2;
-    delay(500);
-  }
-  old_val2 = val2; 
-  if (state2 == 1)
-  {
-    digitalWrite(pinled2, HIGH); 
-  }
-  else
-  {
-    digitalWrite(pinled2, LOW); 
-  }
+  // val2 = ultraleft.Ranging(CM); 
+  // if ((val2 <= 15) && (old_val2 <= 15))
+  // {
+  //   state2 = 1 - state2;
+  //   delay(500);
+  // }
+  // old_val2 = val2; 
+  // if (state2 == 1)
+  // {
+  //   digitalWrite(pinled2, HIGH); 
+  // }
+  // else
+  // {
+  //   digitalWrite(pinled2, LOW); 
+  // }
 }
